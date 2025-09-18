a1358373e40aaccf7387318c5758947dd0cdd006
seikhkabir <seikh.kabir@gmail.com>
2025-09-18 12:28:06 +1000

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -1,8 +1,3 @@
-"""
-Account Service
-
-This microservice handles the lifecycle of Accounts
-"""
 # pylint: disable=unused-import
 from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
 from service.models import Account
@@ -60,11 +55,9 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-    ######################################################################
-    # LIST ALL ACCOUNTS
-    ######################################################################
-    @app.route("/accounts", methods=["GET"])
-    def list_accounts():
+
+ @app.route("/accounts", methods=["GET"])
+ def list_accounts():
         """
         List all Accounts
         This endpoint will list all Accounts
@@ -83,8 +76,8 @@ def create_accounts():
 ######################################################################
 
    
-    @app.route("/accounts/<int:account_id>", methods=["GET"])
-    def get_accounts(account_id):
+  @app.route("/accounts/<int:account_id>", methods=["GET"])
+  def get_accounts(account_id):
         """
         Reads an Account
         This endpoint will read an Account based the account_id that is requested
@@ -101,11 +94,8 @@ def create_accounts():
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-    ######################################################################
-    # UPDATE AN EXISTING ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["PUT"])
-    def update_accounts(account_id):
+  @app.route("/accounts/<int:account_id>", methods=["PUT"])
+  def update_accounts(account_id):
         """
         Update an Account
         This endpoint will update an Account based on the posted data
@@ -125,17 +115,9 @@ def create_accounts():
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
-    ######################################################################
-    # DELETE AN ACCOUNT
-    ######################################################################
    
-    @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-    def delete_accounts(account_id):
-        """
-        Delete an Account
-        This endpoint will delete an Account based on the account_id that is requested
-        """
+  @app.route("/accounts/<int:account_id>", methods=["DELETE"])
+  def delete_accounts(account_id):
         app.logger.info("Request to delete an Account with id: %s", account_id)
 
         account = Account.find(account_id)
@@ -148,7 +130,6 @@ def create_accounts():
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
 
 def check_content_type(media_type):
     """Checks that the media type is correct"""
